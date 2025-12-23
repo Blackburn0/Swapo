188d02c6caede67ab07388a60ca890959f539aa3
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-12-23 12:27:22 +0100

    feat: update service worker revision for cache management

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2vp3jdh6gdo"
+    "revision": "0.k81kn2r9vog"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
